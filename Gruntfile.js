a652c7885e3b560e2fdeafa19d012d50af3e6887
Jeff Hubbard <lord2800@gmail.com>
2013-12-21 14:18:27 -0800

    Basic layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,47 @@
 module.exports = function (grunt) {
-	['phplint', 'php-analyzer', 'phpunit', 'php', 'parallelize', 'gh-pages'].forEach(function (name) { grunt.loadNpmTasks('grunt-' + name); });
+	['phpcs', 'phplint', 'php-analyzer', 'phpunit', 'php', 'parallelize', 'gh-pages', 'contrib-watch']
+		.forEach(function (name) { grunt.loadNpmTasks('grunt-' + name); });
 
+	grunt.initConfig({
+		php: {
+			app: { options: { port: 8080, base: 'public', open: true, keepalive: false } }
+		},
+		phpcs: {
+			app: { dir: 'src' },
+			options: { bin: 'vendor/bin/phpcs', standard: 'PSR1' }
+		},
+		phplint: {
+			options: { swapPath: '/tmp' },
+			app: ['src/**/*.php', 'index.php']
+		},
+		phpunit: {
+			unit: { dir: 'tests/' },
+			options: {
+				bin: 'vendor/bin/phpunit',
+				bootstrap: 'tests/Bootstrap.php',
+				colors: true,
+				testdox: true
+			}
+		},
+		php_analyzer: {
+			options: { bin: 'vendor/bin/phpalizer' },
+			app: { dir: 'src' }
+		},
+		watch: {
+			test: {
+				files: ['tests/**/*.php'],
+				tasks: ['phplint', 'phpunit']
+			},
+			livereload: {
+				options: { livereload: true },
+				files: ['src/**/*.php'],
+				tasks: []
+			}
+		}
+	});
+
+	grunt.registerTask('precommit', ['phplint', 'phpunit']);
+	grunt.registerTask('test', ['phplint', 'phpcs', 'phpunit', 'php_analyzer']);
+	grunt.registerTask('serve', ['php', 'watch']);
+	grunt.registerTask('default', ['phplint', 'phpcs']);
 };
